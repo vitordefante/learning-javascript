a082daa909f5a9abf35215bed025e2cb026500cd
Joao Vitor Defante <joaovitordefante@hotmail.com>
2022-03-16 21:22:47 -0300

    working on exercise

--- Curso em Video - JavaScript/ex009/ex009.js
+++ Curso em Video - JavaScript/ex009/ex009.js
@@ -1,12 +1,18 @@
 
-
 function datetime1() {
     var agora = new Date()
     var diasem = agora.getDay()
-    var mes = agora.getMonth()
+    var mes = agora.getUTCMonth()
     var ano = agora.getFullYear()
+    var horas = agora.getHours()
+    var minutos = agora.getMinutes()
+    var diames = agora.getDate()
 
-    console.log('')
+    var timeinpage = window.document.getElementById('time')
+    var todayinfopage = window.document.getElementById('dia-semana')
+
+
+    
     switch(diasem) {
         case 0:
             diasem = 'Domingo'
@@ -31,17 +37,62 @@ function datetime1() {
             break
     }
 
-    console.log(mes)
-    console.log(diasem)
-    console.log(ano)
+    switch(mes) {
+        case 0:
+            mes = 'Janeiro'
+            break
+        case 1:
+            mes = 'Fevereiro'
+            break
+        case 2:
+            mes = 'Março'
+            break
+        case 3:
+            mes = 'Abril'
+            break
+        case 4:
+            mes = 'Maio'
+            break
+        case 5:
+            mes = 'Junho'
+            break
+        case 6:
+            mes = 'Julho'
+            break
+        case 7:
+            mes = 'Agosto'
+            break
+        case 8:
+            mes = 'Setembro'
+            break
+        case 9:
+            mes = 'Outubro'
+            break
+        case 10:
+            mes = 'Novembro'
+            break
+        case 11:
+            mes = 'Dezembro'
+            break
+    }
 
+    var currenttime = `${horas}:${minutos}`
+    if (minutos < 10) {
+        var currenttime = `${horas}:0${minutos}`
+    } else {
+        var currenttime = `${horas}:${minutos}`
+    }
+    
+    var todayinfo = `${diasem}, ${diames} de ${mes} de ${ano}.`
 
+    timeinpage.innerText = currenttime
+    todayinfopage.innerText = todayinfo
+
+    console.log(todayinfo)
 }
 
 datetime1()
 
 /* 
- * Transformar o mes de numero pra dia escrito.
-
- * Criar uma variavel com uma string contendo o formato de data do site, e fazer ela aparecer no html.
+ * Fazer com que as imagens de fundo mudem de acordo com o horario.
 */
