87f81fa6495d8ab3a0edac508ce04ce8fe833e53
vitordefante <joaovitordefante@hotmail.com>
2022-12-11 18:12:01 -0300

    Made a double space remover little project

---
+++ FreeCodeCamp/functest.js
@@ -0,0 +1,6 @@
+function addString (string) {
+    const str = `This string will be added the parameter = ${string}`;
+    console.log(str);
+}
+
+addString("zapzap");
---
+++ double-space-remover/script.js
@@ -0,0 +1,10 @@
+var spaces = 0;
+
+function spaceRemover() {
+    var usertxt = document.getElementById('text-input');
+    var usertxt1 = usertxt.value
+
+    var nospace = usertxt1.replace(/\s\s+/g, ' ');
+    usertxt.value = nospace;
+
+}
